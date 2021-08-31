3b4b2ce1ae046e35e2f43de9d3462579a425ac09
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-08-31 16:26:21 -0500

    save

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/"
 const starterContainer = document.querySelector(".starter-container");
-// const starterImg = document.createElement("div");
-// starterImg.classList.add(".pokemon-img")
-// starterContainer.append(starterImg)
-// const starterName = document.createElement("div");
-// starterName.classList.add(".pokemon-name")
-// starterContainer.append(starterName)
-
-// const starterName = document.querySelector(".pokemon-name");
-
-
 
 
 //CREATE 
-// function appendImg(url) {
-//   let starterImg = document.createElement("div");
-//   starterImg.src = url;
-//   starterImg.classList.add(".pokemon-img");
-//   starterContainer.append(starterImg);
-// };
+function appendImg(url) {
+  let Img = document.createElement("img");
+  let starterImg = document.createElement("div");
+  Img.src = url;
+  starterImg.classList.add(".pokemon-img");
+  starterContainer.append(starterImg);
+  starterImg.append(Img);
+};
 
-// function appendName(text) {
-//   let starterName = document.createElement("div");
-//   starterName.innerHTML = text;
-//   starterName.classList.add(".pokemon-name");
-//   starterContainer.append(starterName);
-// }
+function appendName(text) {
+  let h3 = document.createElement("h3");
+  let starterName = document.createElement("div");
+  h3.innerHTML = text;
+  starterName.appendChild(h3)
+  starterName.classList.add(".pokemon-name");
+  starterContainer.append(starterName);
+}
 
 //GRASS
 const grassBtn = document.querySelector("#grassBtn");
@@ -36,29 +30,15 @@ grassBtn.addEventListener("click", () => {
 
 async function grassPokemon() {
 
-  let grassType = [810, 495, 650, 722, 387, 152, 252, 1];
+  let grassType = [810, 495, 650, 722, 25, 387, 152, 252, 1, 810, 495, 650, 722, 387, 152, 252, 1];
   let randomGrass = Math.floor(Math.random() * grassType.length);
   let randomPokemonID = grassType[randomGrass];
-
-  const starterImg = document.createElement("div");
-  starterImg.classList.add(".pokemon-img")
-  starterContainer.append(starterImg)
-  const starterName = document.createElement("div");
-  starterName.classList.add(".pokemon-name")
-  starterContainer.append(starterName)
 
   try {
     let res = await axios.get(`${URL}${randomPokemonID}`);
 
-    let grassImg = document.createElement("img");
-    grassImg.src = res.data.sprites.front_default;
-    starterImg.append(grassImg);
-    starterContainer.append(starterImg);
-
-    let grassName = document.createElement("h3");
-    grassName.innerText = res.data.name;
-    starterName.append(grassName);
-    starterContainer.append(starterName);
+    appendImg(res.data.sprites.front_default);
+    appendName(res.data.name);
 
   } catch (error) {
     console.log(error);
@@ -74,29 +54,15 @@ fireBtn.addEventListener("click", () => {
 
 async function firePokemon() {
 
-  let fireType = [813, 498, 653, 725, 390, 155, 255, 4];
+  let fireType = [813, 498, 653, 25, 725, 390, 155, 255, 4, 813, 498, 653, 725, 390, 155, 255, 4];
   let randomFire = Math.floor(Math.random() * fireType.length);
   let randomPokemonID = fireType[randomFire];
-
-  const starterImg = document.createElement("div");
-  starterImg.classList.add(".pokemon-img")
-  starterContainer.append(starterImg)
-  const starterName = document.createElement("div");
-  starterName.classList.add(".pokemon-name")
-  starterContainer.append(starterName)
 
   try {
     let res = await axios.get(`${URL}${randomPokemonID}`);
 
-    let fireImg = document.createElement("img");
-    fireImg.src = res.data.sprites.front_default;
-    starterImg.append(fireImg);
-    starterContainer.append(starterImg);
-
-    let fireName = document.createElement("h3");
-    fireName.innerText = res.data.name;
-    starterName.append(fireName);
-    starterContainer.append(starterName);
+    appendImg(res.data.sprites.front_default);
+    appendName(res.data.name);
 
   } catch (error) {
     console.log(error);
@@ -112,29 +78,15 @@ waterBtn.addEventListener("click", () => {
 
 async function waterPokemon() {
 
-  let waterType = [816, 501, 656, 728, 393, 158, 258, 7];
+  let waterType = [816, 501, 656, 728, 393, 25, 158, 258, 7, 816, 501, 656, 728, 393, 158, 258, 7];
   let randomWater = Math.floor(Math.random() * waterType.length);
   let randomPokemonID = waterType[randomWater];
-
-  const starterImg = document.createElement("div");
-  starterImg.classList.add(".pokemon-img")
-  starterContainer.append(starterImg)
-  const starterName = document.createElement("div");
-  starterName.classList.add(".pokemon-name")
-  starterContainer.append(starterName)
 
   try {
     let res = await axios.get(`${URL}${randomPokemonID}`);
 
-    let waterImg = document.createElement("img");
-    waterImg.src = res.data.sprites.front_default;
-    starterImg.append(waterImg);
-    starterContainer.append(starterImg);
-
-    let waterName = document.createElement("h3");
-    waterName.innerText = res.data.name;
-    starterName.append(waterName);
-    starterContainer.append(starterName);
+    appendImg(res.data.sprites.front_default);
+    appendName(res.data.name);
 
   } catch (error) {
     console.log(error);
