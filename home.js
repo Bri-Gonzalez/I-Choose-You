ca1502c32233aafa7ad61f270a0e22ff824d89d1
Briana Gonzalez <brianagonzalez@Brianas-MacBook-Air.local>
2021-11-13 11:20:56 -0600

    changed title below apps and cleaned up home code

--- home.js
+++ home.js
@@ -1,17 +1,17 @@
-const iChooseYouBtn = document.querySelector("#i-choose-you");
+const iChooseYouBtn = document.querySelector(".i-choose-you");
 const iChooseYouURL = "./starter.html";
 iChooseYouBtn.addEventListener("click", () => {
   window.open(iChooseYouURL, "_self");
 });
 
-const pokeresearchBtn = document.querySelector("#pokeresearch");
+const pokeresearchBtn = document.querySelector(".pokeresearch");
 const pokeresearchURL = "./dex.html";
 pokeresearchBtn.addEventListener("click", () => {
   window.open(pokeresearchURL, "_self");
 });
 
-const pokebinderBtn = document.querySelector("#pokebinder");
-const pokebinderURL = "https://flamboyant-goldwasser-1c3004.netlify.app/ ";
+const pokebinderBtn = document.querySelector(".pokebinder");
+const pokebinderURL = "https://pokebinder.netlify.app/";
 pokebinderBtn.addEventListener("click", () => {
   window.open(pokebinderURL, "_self");
 });
